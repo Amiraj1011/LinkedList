dd460522f781459fa14078120f33fb9aa410358a
“AMIR <amiraj1011@gmail.com>
2021-10-27 12:18:04 -0700

    [ amir UC3 for to create linked list by appending 30 and 70 to 56 ]

--- LinkedList.cs
+++ LinkedList.cs
@@ -66,5 +66,25 @@ namespace LinkedList
             Console.WriteLine("{0} inserted into linked list", node.data);
 
         }
+        //creating append method
+        internal void Append(int data)       
+        {
+            //creating a node object of the Node class
+            Node node = new Node(data);     
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} appended into linked list", node.data);
+        }
     }
 }
